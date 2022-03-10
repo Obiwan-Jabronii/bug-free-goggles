ff9a50e3f2f27dac81622a46297c64a7d9b3208a
Obi Wan Jabronii <mitchell_lough@yahoo.com>
2022-03-10 08:37:14 -0500

    bug fixed page template and added to readme

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # bug-free-goggles
+
+## Description 
+
+A team profile generator that will create cards dynamically based on the users input into the node terminal. It will then generate an HTML to display the cards and show the entire team profile.
+
+## Technologies used 
+
+* JavaScript
+* Inquirer
+* Node 
+* HTML
+
+## Usage 
+
+User opens terminal and runs node for index.js. They are then prompted on type of employee, employee number, email address, etc. After finishing adding member to the team an HTML page will be generated to display the teams cards
+
+##
--- src/page-template.js
+++ src/page-template.js
@@ -81,11 +81,7 @@ const generateManager = employeesArr => {
             <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         </head>
         <body>
-            <header>
-                <div class="container flex-row justify-space-between align-center py-3">
-                    <h1 class="page-title text-light bg-danger bg-gradient py-2 px-3> Your Team </h1>
-                </div>
-            </header>
+
             <main class="container my-5">
                 ${generateManager(employees)}
                 ${generateEngineer(employees)}
